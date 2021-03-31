3a7610d90129a0850ab23274b14d2c8692b66fff
Augusto Maza <debianmaza@gmail.com>
2021-03-31 14:33:49 -0300

    fix navbar buttons #2

--- index.php
+++ index.php
@@ -264,6 +264,8 @@
             <img src="img/devel-slack-logo2-32x32.png"  class="img-reponsive img-rounded"> Slackzone Development
         </button>
        
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
